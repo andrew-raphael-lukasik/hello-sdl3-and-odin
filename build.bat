b52cd844496d1dbe5b77cae3f641653dd9d55870
DESKTOP-SNR3USE\andre <andrew.r.lukasik@gmail.com>
2025-03-14 20:38:32 +0100

    compilation error break points

--- build.bat
+++ build.bat
@@ -2,9 +2,12 @@ set project_name=hello-sdl3
 
 md %~dp0shaders_compiled 2> nul
 glslc ./shaders/shader.glsl.vert -o ./shaders_compiled/shader.spv.vert
+if %errorlevel% neq 0 exit /b 1
 glslc ./shaders/shader.glsl.frag -o ./shaders_compiled/shader.spv.frag
+if %errorlevel% neq 0 exit /b 1
 
 odin build ./source/ -debug
+if %errorlevel% neq 0 exit /b 1
 md %~dp0bin\debug 2> nul
 move source.exe %~dp0bin\debug\%project_name%.exe
 move source.pdb %~dp0bin\debug\%project_name%.pdb
