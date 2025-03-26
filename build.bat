644a84032dbaa054c51a86d6995c3a04dd7e4901
DESKTOP-SNR3USE\andre <andrew.r.lukasik@gmail.com>
2025-03-26 21:03:27 +0100

    switched to "bin" sub-dir for binaries

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 set project_name=hello-sdl3
+set build_name=win64-debug
 
 md %~dp0shaders_compiled 2> nul
 glslc ./shaders/shader.glsl.vert -o ./shaders_compiled/shader.spv.vert
@@ -8,16 +9,14 @@ if %errorlevel% neq 0 exit /b 1
 
 odin build ./source/ -debug
 if %errorlevel% neq 0 exit /b 1
-md %~dp0bin\debug 2> nul
-move source.exe %~dp0bin\debug\%project_name%.exe
-move source.pdb %~dp0bin\debug\%project_name%.pdb
-copy %~dp0source\render\redistributable_bin\SDL3.dll %~dp0bin\debug\
-copy %~dp0source\steam\steamworks\redistributable_bin\win64\steam_api64.dll %~dp0bin\debug\
-copy %~dp0source\steam\steam_appid.txt %~dp0bin\debug\
+set build_path=%~dp0build\%build_name%
+md %build_path%\bin 2> nul
+move source.exe %build_path%\bin\%project_name%.exe
+move source.pdb %build_path%\bin\%project_name%.pdb
+copy %~dp0source\render\redistributable_bin\SDL3.dll %build_path%\bin\
+copy %~dp0source\steam\steamworks\redistributable_bin\win64\steam_api64.dll %build_path%\bin\
+copy %~dp0source\steam\steam_appid.txt %build_path%\bin\
 
 if "%~1" == "run" (
-    %~dp0bin\debug\%project_name%.exe
-    @REM cd bin
-    @REM %project_name%.exe
-    @REM cd ..
+    %build_path%\bin\%project_name%.exe
 )
