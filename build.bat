2020ee368d9e9257b8539bd3b11f8e96aad2438d
DESKTOP-SNR3USE\andre <andrew.r.lukasik@gmail.com>
2025-03-27 17:00:10 +0100

    3 more files to build dir

--- build.bat
+++ build.bat
@@ -2,11 +2,14 @@ set project_name=hello-sdl3
 set build_name=win64-debug
 set build_path=%~dp0build\%build_name%
 
+@REM TODO: at some point figure out a better way of preparing data files
 md %build_path%\data 2> nul
 glslc ./shaders/default_shader.glsl.vert -o %build_path%/data/default_shader.spv.vert
 if %errorlevel% neq 0 exit /b 1
 glslc ./shaders/default_shader.glsl.frag -o %build_path%/data/default_shader.spv.frag
 if %errorlevel% neq 0 exit /b 1
+copy %~dp0assets\default_cube.gltf %build_path%\data\
+copy %~dp0assets\default_texture.png %build_path%\data\
 
 odin build ./source/ -debug
 if %errorlevel% neq 0 exit /b 1
@@ -14,6 +17,7 @@ md %build_path%\bin 2> nul
 move source.exe %build_path%\bin\%project_name%.exe
 move source.pdb %build_path%\bin\%project_name%.pdb
 copy %~dp0source\render\redistributable_bin\SDL3.dll %build_path%\bin\
+copy %~dp0source\render\redistributable_bin\SDL3_image.dll %build_path%\bin\
 copy %~dp0source\steam\steamworks\redistributable_bin\win64\steam_api64.dll %build_path%\bin\
 copy %~dp0source\steam\steam_appid.txt %build_path%\bin\
 
